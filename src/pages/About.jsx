22406c735e4a345a392fac4168238fa01528c0c5
kathrynhoule <ryn.houle@gmail.com>
2025-09-08 18:58:08 -0400

    sets up initial components

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+          <div>
+               <h1>About Me</h1>
+               <p>This is some stuff about me!</p>
+          </div>
+  )
+}
+
+export default About
