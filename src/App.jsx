3022d3cbbd00e18214f2c63fbc33dbe6a6330ee6
kathrynhoule <ryn.houle@gmail.com>
2025-09-09 09:15:11 -0400

    adds styling for the navbar, temporary and may change later

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,19 @@ import About from './pages/About'
 import Projects from './pages/Projects'
 import Contact from './pages/Contact'
 import React from 'react'
+import './App.css';
 
 const App = () => {
   return (
      <div className='app-container'>
-          <nav className='sidebar'>
+          <nav className='navbar'>
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
             <Link to="/projects">Projects</Link>
             <Link to="/contact">Contact</Link>
           </nav>
 
-          <main className='content'>
+          <main className='main-content'>
             <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
