7c55b454d6a070544bbadc70ea867251fad76837
kathrynhoule <ryn.houle@gmail.com>
2025-09-09 08:45:02 -0400

    adds vars file with fonts and temporary palette

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,26 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Projects from './pages/Projects'
 import Contact from './pages/Contact'
-
 import React from 'react'
 
 const App = () => {
   return (
-     <div>
-          <nav>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/projects">Projects</Link>
-          <Link to="/contact">Contact</Link>
+     <div className='app-container'>
+          <nav className='sidebar'>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Contact</Link>
           </nav>
 
-          <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-          </Routes>
+          <main className='content'>
+            <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </main>
      </div>
   )
 }
