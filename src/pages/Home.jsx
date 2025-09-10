dd90743900beffeb512fbabdbcefcf93fafabe5c
kathrynhoule <ryn.houle@gmail.com>
2025-09-10 09:31:41 -0400

    maps home link component to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import '../styles/Home.css';
 import { Link } from 'react-router-dom';
+import HomeLink from '../components/HomeLink';
+import homeLinks from '../data/homeLinks';
 
 const Home = () => {
   return (
@@ -13,29 +15,9 @@ const Home = () => {
             <img src='/assets/temp-avatar.png' alt='Avatar' />
           </div>
 
-          <Link to='/about' className='home-link about-link'>
-            <img src='/assets/temp-projects-icon.png' alt='About' />
-            <div className='link-text'>
-              <h3>About Me</h3>
-              <p>Learn more about me</p>
-            </div>
-          </Link>
-
-          <Link to='/projects' className='home-link projects-link'>
-            <img src='/assets/temp-projects-icon.png' alt='Projects' />
-            <div className='link-text'>
-              <h3>Projects</h3>
-              <p>Check out my work and side projects</p>
-            </div>
-          </Link>
-
-          <Link to='/contact' className='home-link contact-link'>
-              <img src='/assets/temp-projects-icon.png' alt='Contact' />
-            <div className='link-text'>
-              <h3>Contact</h3>
-              <p>Get in touch or see me elsewhere</p>
-            </div>
-          </Link>
+          {homeLinks.map((link, index) => (
+            <HomeLink key={index} {...link} />
+          ))}
         </div>
   )
 }
