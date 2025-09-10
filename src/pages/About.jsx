e846ebf07c417320cafa564af020a3bfbca4414f
kathrynhoule <ryn.houle@gmail.com>
2025-09-10 12:57:48 -0400

    adds footer to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,24 +1,30 @@
 import React from 'react'
 import '../styles/About.css';
+import Footer from '../components/Footer';
 
 const About = () => {
   return (
           <div className='about-container'>
-                    <div className='intro-container'>
-                      <h1>Hi, I'm Ryn!</h1>
-                    </div>
-          
-                    <div className='about-avatar-container'>
-                      <img src='/assets/temp-avatar-about.png' alt='Avatar About' />
-                    </div>
-          
-                    <div className='about-block'>
-                      <div className='about-me'>
-                        <h3>About Me</h3>
-                        <p>Placeholder text</p>
-                      </div>
-                    </div>
-                  </div>
+            <div className='intro-container'>
+              <h1>Hi, I'm Ryn!</h1>
+            </div>
+  
+            <div className='about-avatar-container'>
+              <img src='/assets/temp-avatar-about.png' alt='Avatar About' />
+            </div>
+  
+            <div className='about-block'>
+              <div className='about-me'>
+                <h3>About Me</h3>
+                <p>Placeholder text</p>
+              </div>
+            </div>
+
+            <div className="about-footer" style={{ gridColumn: '1 / 4', gridRow: '6 / 7' }}>
+              <Footer />
+            </div>
+
+          </div>
   )
 }
 
