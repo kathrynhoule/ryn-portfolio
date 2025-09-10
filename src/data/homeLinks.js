8681f4c0a2e388e0196b52b706d734ea4846fcc8
kathrynhoule <ryn.houle@gmail.com>
2025-09-10 09:28:53 -0400

    adds data file for home links

---
+++ src/data/homeLinks.js
@@ -0,0 +1,28 @@
+const homeLinks = [
+     {
+          to: '/about',
+          icon: '/assets/temp-projects-icon.png',
+          alt: 'About',
+          title: 'About Me',
+          description: 'Learn more about me',
+          className: 'about-link',
+     },
+     {
+          to: '/projects',
+          icon: '/assets/temp-projects-icon.png',
+          alt: 'Projects',
+          title: 'Projects',
+          description: 'Check out my work and side projects',
+          className: 'projects-link',
+     },
+     {
+          to: '/contact',
+          icon: '/assets/temp-projects-icon.png',
+          alt: 'Contact',
+          title: 'Contact',
+          description: 'Get in touch or see me elsewhere',
+          className: 'contact-link',
+     },
+];
+
+export default homeLinks;
