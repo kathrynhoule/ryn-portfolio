75772ac9b8b73d616fd45acdea23460804b94366
kathrynhoule <ryn.houle@gmail.com>
2025-09-11 09:15:08 -0400

    adds data for avatar component

---
+++ src/data/avatarData.js
@@ -0,0 +1,45 @@
+const avatarData = {
+     home: {
+     avatar: '/assets/temp-avatar.png',
+     facts: [
+          'React Dev',
+          'Creative coder & designer',
+          'Based in Canada',
+     ],
+     icons: [
+          { src: '/assets/temp-projects-ion.png', alt: 'GitHub'},
+          { src: '/assets/temp-projects-ion.png', alt: 'GitHub'},
+          { src: '/assets/temp-projects-ion.png', alt: 'GitHub'},
+     ],
+     },
+
+     about: {
+     avatar: '/assets/temp-avatar-about.png',
+     facts: [
+          'Lifelong learner',
+          'Love to travel',
+          'Coffee enthusiast',
+     ],
+     icons: [
+          { src: '/assets/temp-projects-ion.png', alt: 'GitHub'},
+          { src: '/assets/temp-projects-ion.png', alt: 'GitHub'},
+          { src: '/assets/temp-projects-ion.png', alt: 'GitHub'},
+     ],
+     },
+
+     contact: {
+     avatar: '/assets/temp-avatar.png',
+     facts: [
+          'Always happy to connect',
+          'Open to collaborations',
+          'Let’s build something great',
+     ],
+     icons: [
+          { src: '/assets/temp-projects-ion.png', alt: 'GitHub'},
+          { src: '/assets/temp-projects-ion.png', alt: 'GitHub'},
+          { src: '/assets/temp-projects-ion.png', alt: 'GitHub'},
+     ],
+     },
+};
+
+export default avatarData;
