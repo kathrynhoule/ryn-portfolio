d052642db2cfb8892c93c5c64b15e935fa7c39a8
kathrynhoule <ryn.houle@gmail.com>
2025-09-11 13:01:19 -0400

    adds typing animation for heading on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../styles/Home.css';
 import HomeLink from '../components/HomeLink';
 import homeLinks from '../data/homeLinks';
@@ -6,10 +6,31 @@ import avatarData from '../data/avatarData';
 import AvatarCard from '../components/AvatarCard';
 
 const Home = () => {
+  const fullText = "Welcome to my portfolio!";
+  const [displayedText, setDisplayedText] = useState("");
+
+  useEffect(() => {
+    const typingDelay = 1000;
+    const charDelay = 100;
+
+    const typeCharacter = (index) => {
+      if (index < fullText.length) {
+        setDisplayedText((prev) => prev + fullText[index]);
+        setTimeout(() => typeCharacter(index + 1), charDelay);
+      }
+    };
+
+    const startTyping = setTimeout(() => {
+      typeCharacter(0);
+    }, typingDelay);
+
+    return () => clearTimeout(startTyping);
+  }, []);
+
   return (
         <div className='home-container'>
           <div className='welcome-container'>
-            <h1>Welcome to my portfolio!</h1>
+            <h1>{displayedText}</h1>
           </div>
 
           <AvatarCard
