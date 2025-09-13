bb9a3f8b51b0ac9276960c9bf61311da1afaf028
kathrynhoule <ryn.houle@gmail.com>
2025-09-13 18:22:18 -0400

    updates styling for contact cards

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -4,9 +4,12 @@ const ContactCard = ({ title, description, icon, alt, link, linkText }) => {
   return (
      <div className='contact-card'>
           <img src={icon} alt={alt} />
-          <div>
-               <h3>{title}</h3>
-               <p>{description}
+          <div className="contact-card-text">
+               <div className="contact-card-header">
+                    <h3>{title}</h3>
+               </div>
+               <p>
+                    {description}{' '}
                     <a href={link} target='_blank' rel='noopener noreferrer'>
                          {linkText}
                     </a>
