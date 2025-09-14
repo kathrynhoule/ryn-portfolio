a71ad9ad9479de2bd3e25119073b9061238ea65e
kathrynhoule <ryn.houle@gmail.com>
2025-09-13 20:23:30 -0400

    updates data and styling for project cards

--- src/data/projectCards.js
+++ src/data/projectCards.js
@@ -28,14 +28,14 @@ const projectCards = [
      {
           image: '/assets/temp-project.png',
           alt: 'Project 3',
-          title: 'Project 3',
-          description: 'Here\'s some stuff about this project. Check it out on ',
-          link: 'https://github.com/myusername/project3',
+          title: 'Classroom Game & Quiz App',
+          description: 'This was an application that I worked on for a small company called AgiLearning, intended for university-level classroom use. Check out the repo on ',
+          link: 'https://github.com/agilearning-plugin/agilearning-plugin/tree/phase-1',
           linkText: 'GitHub',
           icons: [
                { src: '/assets/home-icon-react.png', alt: 'React'},
-               { src: '/assets/home-icon-python.png', alt: 'Python'},
-               { src: '/assets/home-icon-aseprite.png', alt: 'Aseprite'},
+               { src: '/assets/temp-projects-icon.png', alt: 'MySQL'},
+               { src: '/assets/temp-projects-icon.png', alt: 'Heroku'},
           ],
      },
 ];
