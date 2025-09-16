d1d0a3f324d2625c33a83fcc9e7039dfa530a7ac
kathrynhoule <ryn.houle@gmail.com>
2025-09-16 14:19:28 -0400

    updates main projects page styling

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,14 +3,16 @@ import React from 'react'
 const ProjectCard = ({ image, alt, title, description, link, linkText, icons }) => {
   return (
      <div className='project-card'>
-          <img src={image} alt={alt} className='project-image'/>
-          <div className='project-text'>
-               <h2>{title}</h2>
-               <p>{description}
-                    <a href={link} target='_blank' rel='noopener noreferrer'>
-                         {linkText}
-                    </a>
-               </p>
+          <div className='project-card-top'>
+               <img src={image} alt={alt} className='project-image'/>
+               <div className='project-text'>
+                    <h2>{title}</h2>
+                    <p>{description}
+                         <a href={link} target='_blank' rel='noopener noreferrer'>
+                              {linkText}
+                         </a>
+                    </p>
+               </div>
           </div>
           <div className='made-with'>
                <h3>Made with:</h3>
