49c28a5ba9e6996b02601d3d1a3ad07b1531a7aa
kathrynhoule <ryn.houle@gmail.com>
2025-09-22 20:26:09 -0400

    updates icons for side projects

--- src/data/sideProjectCards.js
+++ src/data/sideProjectCards.js
@@ -1,6 +1,6 @@
 const sideProjectCards = [
      {
-          image: '/assets/coming-soon.png',
+          image: '/assets/garden-app-icon.png',
           alt: 'Project 1',
           title: 'Mini Gardening Game',
           description: 'A fun, interactive gardening game that lets players grow and harvest different plants to build a virtual garden in their browser. Still being built, but the repo is up on ',
@@ -12,7 +12,7 @@ const sideProjectCards = [
           ],
      },
      {
-          image: '/assets/coming-soon.png',
+          image: '/assets/pet-app-icon.png',
           alt: 'Project 2',
           title: 'Virtual Pet Game',
           description: 'A browser-based virtual pet game where you feed, play, and care for your pet in a fun, interactive way as they grow . Still being built, but you can view the repo on ',
@@ -25,10 +25,10 @@ const sideProjectCards = [
           ],
      },
      {
-          image: '/assets/coming-soon.png',
+          image: '/assets/site-icon.png',
           alt: 'Project 3',
           title: 'This site!',
-          description: 'The site you are on was made by me with React over the course of about 5 days, and I am still tweaking things and updating info. The repo for this site is public on ',
+          description: 'The site you are on was made by me with React over the course of about 5 days. The icons and images were all made by me using Aseprite at 32x32 or 16x16 size. The repo for this site is public on ',
           link: 'https://github.com/kathrynhoule/ryn-portfolio',
           linkText: 'Github',
           icons: [
